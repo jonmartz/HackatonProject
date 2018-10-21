ec7aa4a4c87def23aa6c809aa5e8bef94656dad9
guys79 <guys797@gmail.com>
2018-10-21 16:07:25 +0300

    implemented delete function in module

--- src/Model/UserDatabase.java
+++ src/Model/UserDatabase.java
@@ -66,7 +66,7 @@ public class UserDatabase implements IModel{
         }
     }
 
-    public void updateUser(String username, String filedToUpdate ,String valueToChange){
+    public void updateUser(String username, String filedToUpdate ,String valueToChange){///Maybe Also need to check if the password is the correct password
         try {
             openConnection();
             Statement statement = connection.createStatement();
@@ -83,6 +83,47 @@ public class UserDatabase implements IModel{
             e.printStackTrace();
         } finally {
             closeConnection();
+        }
+    }
+
+    /**
+     * This function will delete a user from the database
+     * @param username - The username of the user
+     * @param password - The password of the user
+     */
+    public void deleteUser(String username, String password)
+    {
+        User selectedUser=getUser(username); // Get the user
+
+        //If the user dose not exist
+        if(selectedUser==null)
+        {
+            // TODO: 10/21/2018 Add a text that says that this user dose not exist
+
+        }
+        else{//If the user does exist
+
+            if(!selectedUser.password.equals(password)) //If the user gave an incorrect password
+            {
+                // TODO: 10/21/2018 Add a text that says that the password is incorrect
+            }
+            else
+            {
+                openConnection();
+                try {
+                    Statement statement = connection.createStatement();
+                    String command = "delete from users where" +
+                            "username='" + selectedUser.username + "'";
+                    statement.executeUpdate(command);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    closeConnection();
+                }
+
+            }
         }
     }
 
