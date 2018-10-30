100157b93010c434d4e84e1f2f416a37328edd20
guy schlesinger <guys797@gmail.com>
2018-10-30 17:04:52 +0200

    push to master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         IModel userDatabase = new UserDatabase();//Creating the database
         UserView userView = new UserView(primaryStage);
-        userView.mainMenu();//Moving to the mainMenu window
+        userView.mainMenu();//Moving to the mainMenu window.
         userView.setupView(userDatabase);
         primaryStage.show();
     }
