deab67f3f3df36199ba6a314bca706d6be03be0c
guys79 <guys797@gmail.com>
2018-12-16 18:52:08 +0200

    tradeTransaction

--- src/main/java/Controller/AcceptanceMessageController.java
+++ src/main/java/Controller/AcceptanceMessageController.java
@@ -29,7 +29,7 @@ public class AcceptanceMessageController extends MessageController {
             CompletionMessage completionMessage1 = new CompletionMessage(currentMessage.getReceiver(), currentMessage.getSender(), transaction, vacation);
             CompletionMessage completionMessage2 = new CompletionMessage(currentMessage.getSender(), currentMessage.getReceiver(), transaction, vacation);
 
-            this.database.addTransaction(vacation.ID, transaction.buyerID);
+            this.database.addTransaction(transaction);
             this.database.addMessage(completionMessage1.getSender(), completionMessage1.getReceiver(), vacation.ID, false, completionMessage1.getDate(), completionMessage1.getTime(), completionMessage1.getKind());
             this.database.addMessage(completionMessage2.getSender(), completionMessage2.getReceiver(), vacation.ID, false, completionMessage2.getDate(), completionMessage2.getTime(), completionMessage2.getKind());
             currentMessage.setPayed(true);
